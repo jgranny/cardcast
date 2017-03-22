e1acef012952cef95eb0c2164a8b9e368356b0b2
Elijah Schow <elijah.schow@gmail.com>
2017-03-22 16:00:48 -0500

    Remove casting from the main list's controller

--- clients/sender/controllers/main/main.js
+++ clients/sender/controllers/main/main.js
@@ -5,54 +5,11 @@ angular.module('cardcast.main', [])
 
   // Set $scope.deck with info received from deck resolve
   $scope.deck = deck;
-  $scope.currentDeck = {};
 
   //toggles popup warning using 'ng-show' in main.html
   $scope.showWarning = false;
   $scope.showDelete = false;
   $scope.username = user;
-  $scope.title = 'Untitled';
-
-
-  // First checks for a session and sees if anyone else is currently casting.
-  // Casts the card that invoked it as long as no one else is casting,
-  // otherwise triggers the popup warning.
-  $scope.showPopup = function(card) {
-
-    // if there is an active session and no one is casting, cast the card
-    if (session && !isCasting) {
-      console.log(session.status);
-      $scope.castCard(card);
-
-    // if there is an active session and someone else is casting show popup
-    } else if (session && isCasting) {
-      $scope.showWarning = true;
-      $scope.currentDeck = deck;
-    } else if (chrome.cast) {
-
-    // if there is no active session request one
-      chrome.cast.requestSession(function(currentSession) {
-        sessionListener.call(null, currentSession);
-
-        // Provides extra time for the reciever to respond
-        $timeout(function() {
-          if (!isCasting) {
-            $scope.castCard(deck);
-          } else {
-            $scope.showWarning = true;
-            $scope.currentDeck = deck;
-          }
-        }, 100);
-
-      }, console.log.bind(null, 'onError: '));
-    }
-  };
-
-  // clears popup warning if user cancels the cast
-  $scope.cancelCast = function() {
-    $scope.showWarning = false;
-  };
-
 
   // Deletes selected card from the database
   $scope.deleteDeck = function(deck) {
@@ -64,30 +21,13 @@ angular.module('cardcast.main', [])
       });
   };
 
-  $scope.warnDelete = function(deck) {
+  $scope.warnDelete = function(card) {
     $scope.showDelete = true;
-    $scope.currentDeck = deck;
+    $scope.currentCard = card;
   };
 
   $scope.cancelDelete = function() {
     $scope.showDelete = false;
-  };
-
-
-
-  // Function that creates new deck
-  $scope.createDeck = function() {
-    // Format the card info to match deck model schema
-    var deckInfo = {
-      title: $scope.title,
-    };
-
-    // Use the createCard function from the Service factory
-    Service.createDeck(deckInfo)
-      .then(function(resp) {
-        $location.path('/');
-      });
-
   };
 
 });
