7e614332ce6d7072ca975fc696fd84d76fa41c09
Jonathan Granstaff <jon.granstaff@gmail.com>
2017-03-22 19:10:52 -0500

    Created browserCast.js

---
+++ clients/receiver/browserCast.js
@@ -0,0 +1,33 @@
+angular.module('cardcast-receiver', [
+  'ngSanitize'
+])
+//set up  controller for Receiver.
+.controller('MainController', function($scope, $sanitize, Markdown) {
+
+  var isCasting = false;
+  var who = null;
+  var cardId = null;
+
+  //broadcast makes the receiver send out a response message to all connected senders
+  //it tells them who if anyone is currently casting and the id of the card that is being cast
+  //when the card sender gets this broadcast, main.html changes 'cast' button to 'stop'
+  var broadcast = function() {
+    messageBus.broadcast(JSON.stringify({
+      who: who,
+      isCasting: isCasting,
+      cardId: cardId
+    }));
+  };
+
+  //default message when no one is casting
+  $scope.text = '<h2>Welcome to CardCast!</h2><br/>Nothing has been casted yet...';
+
+})
+.factory('Markdown', function() {
+  var compile = function(text) {
+    return marked(text);
+  };
+  return {
+    compile: compile
+  };
+});
