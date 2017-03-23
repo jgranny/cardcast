06504d16334ee96a7a6b421fa24b6ffd7e788ad0
Elijah Schow <elijah.schow@gmail.com>
2017-03-23 16:39:08 -0500

    Fix deleting cards from a deck

--- clients/sender/controllers/deck/deck.js
+++ clients/sender/controllers/deck/deck.js
@@ -3,10 +3,23 @@ angular.module('cardcast.deck', [
 ])
 
 .controller('DeckCtrl', function($scope, $location, $routeParams, $sanitize, Service, deck) {
+<<<<<<< HEAD
   $scope.deck = deck
 
   $scope.setCurrent = function(card) {
     Service.setCurrent(card)
       .then(res=> console.log(res))
   }
+=======
+  $scope.deck = deck;
+
+  // Deletes selected card from the database
+  $scope.deleteCard = function(card) {
+    Service.deleteCard(card)
+      .then(function(resp) {
+        var index = $scope.deck.cards.indexOf(card);
+        $scope.deck.cards.splice(index, 1);
+      });
+  };
+>>>>>>> Fix deleting cards from a deck
 });
