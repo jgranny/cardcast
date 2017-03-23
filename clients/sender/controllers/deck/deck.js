a4a527637a9a5e47ea4e0fd529792cc9920ad9af
Elijah Schow <elijah.schow@gmail.com>
2017-03-23 12:07:59 -0500

    Update deck editor

--- clients/sender/controllers/deck/deck.js
+++ clients/sender/controllers/deck/deck.js
@@ -2,18 +2,20 @@ angular.module('cardcast.deck', [
   'ngSanitize'
 ])
 
-.controller('DeckCtrl', function($scope, $location, $routeParams, $sanitize, Service, deck) {
-  $scope.deck = deck
-
-  $scope.cards = [
-  {
-  __v:0,
-_id:"58d3f1a5d49b292ddd6953c5",
-card:"test",
-deck:"58d3faf13db0e56e0c3248e2",
-title:"test"
-}, {title: 'B'}, {title: 'C'}];
-
-  $scope.currentCard = {};
-  
+.controller('DeckCtrl', function($scope, $location, $routeParams, $sanitize, Service) {
+  $scope.deck = {
+    title: "Deck Title",
+    current: "58d3f1a5d49b292ddd6953c5",
+    cards: [
+      {
+        __v:0,
+        _id:"58d3f1a5d49b292ddd6953c5",
+        card:"test",
+        deck:"58d15a201865f335663a8f78",
+        title:"test"
+      },
+      {title: 'B'},
+      {title: 'C'}
+    ]
+  };
 });
