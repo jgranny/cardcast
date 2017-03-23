707414bf5d90a7a2998777567506fc8f6faada6d
Elijah Schow <elijah.schow@gmail.com>
2017-03-23 12:09:25 -0500

    Auto-indent `service.js`

--- clients/sender/controllers/deck/deck.js
+++ clients/sender/controllers/deck/deck.js
@@ -2,20 +2,21 @@ angular.module('cardcast.deck', [
   'ngSanitize'
 ])
 
-.controller('DeckCtrl', function($scope, $location, $routeParams, $sanitize, Service) {
-  $scope.deck = {
-    title: "Deck Title",
-    current: "58d3f1a5d49b292ddd6953c5",
-    cards: [
-      {
-        __v:0,
-        _id:"58d3f1a5d49b292ddd6953c5",
-        card:"test",
-        deck:"58d15a201865f335663a8f78",
-        title:"test"
-      },
-      {title: 'B'},
-      {title: 'C'}
-    ]
-  };
+.controller('DeckCtrl', function($scope, $location, $routeParams, $sanitize, Service, deck) {
+  $scope.deck = deck;
+  // $scope.deck = {
+  //   title: "Deck Title",
+  //   current: "58d3f1a5d49b292ddd6953c5",
+  //   cards: [
+  //     {
+  //       __v:0,
+  //       _id:"58d3f1a5d49b292ddd6953c5",
+  //       card:"test",
+  //       deck:"58d15a201865f335663a8f78",
+  //       title:"test"
+  //     },
+  //     {title: 'B'},
+  //     {title: 'C'}
+  //   ]
+  // };
 });
