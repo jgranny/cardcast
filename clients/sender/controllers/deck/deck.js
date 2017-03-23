e4b95f0475efa365f862af0ce041fbf9f387cc03
Elijah Schow <elijah.schow@gmail.com>
2017-03-23 14:48:45 -0500

    Fix add button on the main page

--- clients/sender/controllers/deck/deck.js
+++ clients/sender/controllers/deck/deck.js
@@ -4,20 +4,5 @@ angular.module('cardcast.deck', [
 
 .controller('DeckCtrl', function($scope, $location, $routeParams, $sanitize, Service, deck) {
   $scope.deck = deck;
-  console.log(deck);
-  // $scope.deck = {
-  //   title: "Deck Title",
-  //   current: "58d3f1a5d49b292ddd6953c5",
-  //   cards: [
-  //     {
-  //       __v:0,
-  //       _id:"58d3f1a5d49b292ddd6953c5",
-  //       card:"test",
-  //       deck:"58d15a201865f335663a8f78",
-  //       title:"test"
-  //     },
-  //     {title: 'B'},
-  //     {title: 'C'}
-  //   ]
-  // };
+  console.log('EDITING DECK', deck);
 });
