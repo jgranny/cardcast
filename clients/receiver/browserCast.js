495dbfc6f64aba82642f38a197b64f1a1caa1566
Jonathan Granstaff <jon.granstaff@gmail.com>
2017-03-23 19:36:04 -0500

    Long polling implemented

--- clients/receiver/browserCast.js
+++ clients/receiver/browserCast.js
@@ -2,22 +2,33 @@ angular.module('cardcast-receiver', [
   'ngSanitize'
 ])
 //set up  controller for Receiver.
-.controller('MainController', function($scope, $sanitize, Markdown, $http) {
+.controller('MainController', function($scope, $sanitize, Markdown, $http, $timeout) {
   //TODO
   //Send get request to decks to grab current card
     //Change scope.text to current.text??
   //Set up polling function (Also in senders)
-  $http({
-    method: 'GET',
-    url: '/api/decks/:id',
-    // dataType: 'jsonp',
-    headers: {'Content-Type': 'application/json'},
-  }).then(function success(res) {
-    //update the current card being displayed
-    $scope.text = res.data.current.card
-  }, function error(res) {
-    console.error(res)
-  })
+  var newCardRequest = function () {
+    $http({
+      method: 'GET',
+      url: '/api/decks/58d2ec5a63d0c867c5ec779f',
+      // dataType: 'jsonp',
+      headers: {'Content-Type': 'application/json'},
+    }).then(function success(res) {
+      console.info(res.data.current)
+      res.data.cards.forEach((card) => {
+        if (card._id === res.data.current) {
+          $scope.text = card.card;
+
+          $timeout(newCardRequest, 1000);
+        }
+      })
+      //update the current card being displayed
+    }, function error(res) {
+      console.error(res)
+    })
+  };
+
+  newCardRequest();
 
   //default message when no one is casting
   $scope.text = '<h2>Welcome to CardCast!</h2><br/>Nothing has been cast yet...';
