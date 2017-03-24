718eb2c83519298eef84ecddec953351211f4252
Elijah Schow <elijah.schow@gmail.com>
2017-03-24 12:24:15 -0500

    Update the preview when a new card is cast

--- clients/sender/controllers/deck/deck.js
+++ clients/sender/controllers/deck/deck.js
@@ -3,9 +3,10 @@ angular.module('cardcast.deck', [
 ])
 
 .controller('DeckCtrl', function($scope, $location, $routeParams, $sanitize, $sce, Service, deck) {
-  $scope.deck = deck
-  $scope.preview = $sce.trustAsResourceUrl("/receiver/" + deck.current);
+  $scope.deck = deck;
+  $scope.preview = '';
   $scope.setCurrent = function(card) {
+    $scope.deck.current = card._id;
     Service.setCurrent(card)
       .then(res=> console.log("card Casted"))
   }
@@ -18,4 +19,9 @@ angular.module('cardcast.deck', [
         $scope.deck.cards.splice(index, 1);
       });
   };
+
+  $scope.$watch('deck.current', function(newValue, oldValue) {
+    let content = $scope.deck.cards.filter(card => card._id === newValue)[0].card;
+    $scope.preview = $sanitize(Service.markDownCompile(content));
+  });
 });
